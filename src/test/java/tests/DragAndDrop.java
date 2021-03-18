ab39c3bac28eacd33eb61091c8c6ab60fb86dcad
A-Prudnikova <a.prudnikova@gmail.com>
2021-03-18 19:38:33 +0300

    squashed commits 2-4

--- src/test/java/tests/DragAndDrop.java
+++ src/test/java/tests/DragAndDrop.java
@@ -1,24 +1,17 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byAttribute;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class draganddrop {
+public class DragAndDrop {
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
-        Configuration.timeout = 10000;
-    }
+            }
 
     @Test
     void dragAndDrop() {
@@ -26,6 +19,5 @@ public class draganddrop {
         $("#column-a").dragAndDropTo($("#column-b"));
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
-        sleep(5000);
     }
 }
