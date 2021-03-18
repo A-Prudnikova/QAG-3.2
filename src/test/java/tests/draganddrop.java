1724499ec7e6f159fbe81528d6d41d09ac087a53
A-Prudnikova <a.prudnikova@gmail.com>
2021-03-18 19:30:52 +0300

    test upd-1

---
+++ src/test/java/tests/draganddrop.java
@@ -0,0 +1,31 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class draganddrop {
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+        Configuration.timeout = 10000;
+    }
+
+    @Test
+    void dragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        sleep(5000);
+    }
+}
